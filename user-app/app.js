fb647c11b69ac4bf9849e936582a8b38fc5c0762
PavelTitov16 <pashoktitov16@gmail.com>
2024-08-01 00:48:25 +0300

    feat: add pug teamplates

--- user-app/app.js
+++ user-app/app.js
@@ -1,4 +1,5 @@
 const path = require('path');
+
 const express = require('express');
 const bodyParser = require('body-parser');
 
@@ -17,7 +18,7 @@ app.use('/admin', adminData.routes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
-})
+    res.status(404).render('404', {pageTitle: 'Page Not Found'});
+});
 
 app.listen(3000);
