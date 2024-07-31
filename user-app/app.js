e2c16e667d1a937ff9e17575d431ed75eec85d7e
PavelTitov16 <pashoktitov16@gmail.com>
2024-07-31 13:31:42 +0300

    feat: add two routes to app

--- user-app/app.js
+++ user-app/app.js
@@ -1,7 +1,21 @@
 const http = require('http');
 
 const server = http.createServer((req, res) => {
+    const url = req.url;
 
+    if (url === '/') {
+        res.write('<html>');
+        res.write('<head><title>Main Page</title></head>');
+        res.write('<body><h1>Welcome to Node JS!</h1></body>');
+        res.write('</html>');
+    };
+
+    if (url === '/users') {
+        res.write('<html>');
+        res.write('<head><title>Main Page</title></head>');
+        res.write('<body><ul><li>User 1</li><li>User 2</li><li>User 3</li></ul></body>');
+        res.write('</html>');
+    }
 });
 
 server.listen(3000);
