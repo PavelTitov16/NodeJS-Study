5199ec76a4de7af3f77166ff8aeb080e5880d6f4
PavelTitov16 <pashoktitov16@gmail.com>
2024-07-31 17:53:56 +0300

    init: express setup

--- user-app/app.js
+++ user-app/app.js
@@ -1,40 +1,12 @@
 const http = require('http');
+const express = require('express');
 
-const server = http.createServer((req, res) => {
-    const url = req.url;
-    const method = req.method;
+const app = express();
 
-    if (url === '/') {
-        res.write('<html>');
-        res.write('<head><title>Main Page</title></head>');
-        res.write('<body><h1>Welcome to Node JS!</h1><form action="/create-user" method="POST"><input type="text" name="user" placeholder="username"><button type="submit">Create User</button></form></body>');
-        res.write('</html>');
-        return res.end();
-    };
+app.use((req, res, next) => {
+    
+})
 
-    if (url === '/create-user' && method === 'POST') {
-        const body = [];
-
-        req.on('data', (chunk) => {
-            console.log(chunk, 'chunk');
-            body.push(chunk);
-        });
-        req.on('end', () => {
-            const parsedBody = Buffer.concat(body).toString();
-            const user = parsedBody.split('=')[1];
-            console.log(user, 'user');
-        });
-        res.statusCode = 302;
-        res.setHeader('Location', '/');
-        return res.end();
-    };
-
-    if (url === '/users') {
-        res.write('<html>');
-        res.write('<head><title>Main Page</title></head>');
-        res.write('<body><ul><li>User 1</li><li>User 2</li><li>User 3</li></ul></body>');
-        res.write('</html>');
-    }
-});
+const server = http.createServer(app);
 
 server.listen(3000);
