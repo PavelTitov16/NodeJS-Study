269e93cba9dfd42d1450a73c8a3a219ff7781d7f
PavelTitov16 <pashoktitov16@gmail.com>
2024-07-31 19:34:50 +0300

    feat: add parsing requests logic

--- user-app/app.js
+++ user-app/app.js
@@ -1,12 +1,23 @@
-const http = require('http');
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
 
-app.use((req, res, next) => {
-    
-})
+app.use(bodyParser.urlencoded({extended: false}));
 
-const server = http.createServer(app);
+app.use('/add-product', (req, res, next) => {
+    console.log('Second');
+    res.send('<form action="product" method="POST"><input type="text" name="title"><button type="Submit">Submit</button></form>');
+});
 
-server.listen(3000);
+app.use('/product', (req, res, next) => {
+    console.log(req.body);
+    res.redirect('/');
+});
+
+app.use('/', (req, res, next) => {
+    console.log('Always runs');
+    res.send('<h1>Hello Express</h1>');
+});
+
+app.listen(3000);
