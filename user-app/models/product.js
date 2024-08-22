1ac01732eba6c01112c477e61828ef9a579ed721
PavelTitov16 <pashoktitov16@gmail.com>
2024-08-22 12:06:48 +0300

    feat: add product update logic

--- user-app/models/product.js
+++ user-app/models/product.js
@@ -2,18 +2,29 @@ const mongodb = require('mongodb');
 const getDb = require('../util/database').getDb;
 
 class Product {
-  constructor(title, price, imageUrl, description) {
+  constructor(title, price, imageUrl, description, id) {
     this.title = title;
     this.price = price;
     this.imageUrl = imageUrl;
     this.description = description;
+    this._id = new mongodb.ObjectId(id);
   }
 
   save() {
     const db = getDb();
+    let dbOperation;
 
-    return db.collection('products')
-      .insertOne(this)
+    if (this._id) {
+      dbOperation = db
+        .collection('products')
+        .updateOne({ _id: this._id }, { $set: this });
+    } else {
+      dbOperation = db
+        .collection('products')
+        .insertOne(this);
+    }
+
+    return dbOperation
       .then(result => {
         console.log(result);
       })
